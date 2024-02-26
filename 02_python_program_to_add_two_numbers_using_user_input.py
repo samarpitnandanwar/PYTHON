2de9b0db4d640f5dda850170e4c7719ddccee5c1
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-02-26 14:22:47 +0530

    Add files via upload

---
+++ 02_python_program_to_add_two_numbers_using_user_input.py
@@ -0,0 +1,9 @@
+# Python Program to add two numbers by taking user input
+
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+sum = float(a) + float(b)
+
+print("The Sum of ",a," and ",b," is: ",sum)
+
